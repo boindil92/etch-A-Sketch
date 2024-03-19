f1360a0e438245c73364114443ccd1d00cd4fd90
Seba <talahammen@gmail.com>
2024-03-19 16:21:34 +0100

    added aspect ratio on css.

--- etch-a-Sketch.js
+++ etch-a-Sketch.js
@@ -2,27 +2,27 @@ const container=document.querySelector("#container");
 const gridColumn= [];
 const gridRaw=[];
 
-let grid=6
+let grid=9
 
 for(let i=0 ; i<grid ; i++) {
    gridColumn[i] = document.createElement("div");
    gridColumn[i].classList.add("column");
    container.appendChild(gridColumn[i]);
-   // gridColumn[i].textContent="hi";
+
    for (let a=0; a<grid; a++ ) {
       gridRaw[a] = document.createElement("div");
       gridRaw[a].classList.add("raw");
-      gridRaw[a].addEventListener("mouseover", marker);
       gridColumn[i].appendChild(gridRaw[a]);
-     // gridRaw[a].textContent="yo";
-     function marker () {
 
-      gridRaw[a].classList.remove("raw");
-      gridRaw[a].classList.add("passedRaw");
-   
-   }
 
-   }
+    }
 
 }
 
+container.addEventListener("mouseover", marker);
+function marker (event) {
+let target=event.target;  //event.target è una propietà che indica l' elemento appena raggiunto dal mouse
+target.classList.remove("raw");
+target.classList.add("passedRaw");
+      
+}
