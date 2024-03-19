61978852b606b96d0c6b681f1099e6b995359852
Seba <talahammen@gmail.com>
2024-03-19 13:26:49 +0100

    GRID CREATED!!! it works and it is scalable, now it is time to adjust parameters.

--- etch-a-Sketch.js
+++ etch-a-Sketch.js
@@ -1,12 +1,20 @@
 const container=document.querySelector("#container");
-const gridColumn= []
+const gridColumn= [];
+const gridRaw=[];
 
-for(let i=0 ; i<16 ; i++) {
+let grid=6
+
+for(let i=0 ; i<grid ; i++) {
    gridColumn[i] = document.createElement("div");
+   gridColumn[i].classList.add("column");
    container.appendChild(gridColumn[i]);
-   gridColumn[i].textContent="hi";
+   // gridColumn[i].textContent="hi";
+   for (let a=0; a<grid; a++ ) {
+      gridRaw[a] = document.createElement("div");
+      gridRaw[a].classList.add("raw");
+      gridColumn[i].appendChild(gridRaw[a]);
+      gridRaw[a].textContent="yo";
 
-   for (let e=0 ; e<16 ; e++) {
-    
    }
+
 }
