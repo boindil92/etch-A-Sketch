bb91d3ed1780f9b94f87775a06b85cbc5d613bf0
Seba <talahammen@gmail.com>
2024-03-20 22:31:55 +0100

    added a new style and a new js, so now it is possible to change actively the color!

---
+++ etch-a-Sketch-colors.js
@@ -0,0 +1,62 @@
+const superContainer=document.querySelector("#superContainer");
+let play=document.querySelector("#play");
+
+let grid=undefined;
+
+
+
+
+play.addEventListener("click",getPlayerChoice);
+
+function getPlayerChoice (){
+
+    
+
+    grid= prompt("Please enter a number");
+
+    const gridColumn= [];
+    const gridRaw=[];
+    
+
+        
+    let container=document.createElement("div");
+    superContainer.appendChild(container);
+        
+    for(let i=0 ; i<grid ; i++) {
+        gridColumn[i] = document.createElement("div");
+        gridColumn[i].classList.add("column");
+        container.appendChild(gridColumn[i]);
+
+        for (let a=0; a<grid; a++ ) {
+            gridRaw[a] = document.createElement("div");
+            gridRaw[a].classList.add("box");
+            gridColumn[i].appendChild(gridRaw[a]);
+        }
+    }
+
+    
+
+    container.addEventListener("mouseover", marker);
+           
+    function marker (event) {
+
+        const piker=document.querySelector("#piker");
+        piker.addEventListener("input",changecolor);
+        function changecolor() {
+            color=piker.value;
+        }
+
+        let target=event.target; 
+        target.style.backgroundColor=color;
+
+    }
+
+    play.addEventListener("click",clear);
+
+    function clear () {
+        superContainer.removeChild(container);
+    }
+} 
+
+
+ 
