7875dcbe563c9e932353801c0ccd9f418b48c02a
Seba <talahammen@gmail.com>
2024-03-20 18:30:30 +0100

    created and added buttons, not repeat, with refr.

--- etch-a-Sketch.js
+++ etch-a-Sketch.js
@@ -13,12 +13,13 @@ let black="black";
 let pink="pink";
 let blue="blue";
 
-let colorBefore=yellow;
-let colorAfter=green;
+let colorBefore=pink;
+let colorAfter=blue;
 
 
 play.addEventListener("click",getPlayerChoice);
-play.addEventListener("click",createHetchASketch);
+play.addEventListener("click",getColorChoice);
+
 
 
 
@@ -28,6 +29,55 @@ function getPlayerChoice (){
    grid= prompt("Please enter a number");
 
 }
+
+function getColorChoice () {
+   alert("Please, choose one color");
+
+
+   button1=document.createElement("button");
+   button1.textContent="yellow";
+   button1.setAttribute("style", "color: black; background: yellow;");
+   button1.addEventListener("click",toYellow);
+   button1.addEventListener("click",createHetchASketch);
+   superContainer.appendChild(button1);
+
+   button2=document.createElement("button");
+   button2.textContent="green";
+   button2.setAttribute("style", "color: black; background: green;");
+   button2.addEventListener("click",toGreen);
+   button2.addEventListener("click",createHetchASketch);
+   superContainer.appendChild(button2);
+
+   button3=document.createElement("button");
+   button3.textContent="black";
+   button3.setAttribute("style", "color: white; background: black;");
+   button3.addEventListener("click",toBlack);
+   button3.addEventListener("click",createHetchASketch);
+   superContainer.appendChild(button3);
+
+   button4=document.createElement("button");
+   button4.textContent="pink";
+   button4.setAttribute("style", "color: black; background: pink;");
+   button4.addEventListener("click",toPink);
+   button4.addEventListener("click",createHetchASketch);
+   superContainer.appendChild(button4);
+
+}
+
+function toYellow () {
+   colorBefore=yellow;
+
+}
+function toGreen () {
+   colorBefore=green;
+}
+function toBlack () {
+   colorBefore=black;
+}
+function toPink () {
+   colorBefore=pink;
+}
+ 
 
 
 function createHetchASketch () {
@@ -54,6 +104,10 @@ superContainer.appendChild(container);
          }
 
    }
+button1.removeEventListener("click",createHetchASketch);
+button2.removeEventListener("click",createHetchASketch);
+button3.removeEventListener("click",createHetchASketch);
+button4.removeEventListener("click",createHetchASketch);
 
  container.addEventListener("mouseover", marker);
 
@@ -65,8 +119,14 @@ superContainer.appendChild(container);
       
    }
 play.addEventListener("click",clear);
+
 function clear () {
    superContainer.removeChild(container);
+   superContainer.removeChild(button1);
+   superContainer.removeChild(button2);
+   superContainer.removeChild(button3);
+   superContainer.removeChild(button4);
+   
 }
 
 }
